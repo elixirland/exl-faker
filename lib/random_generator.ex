dc5f89372c1d5c31126ba2b9fb4fb429af550125
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-07-13 19:51:16 +0200

    Change default count to module attribute

--- lib/random_generator.ex
+++ lib/random_generator.ex
@@ -1,15 +1,17 @@
 defmodule RandomGenerator do
   alias RandomGenerator.{Titles, Pages}
 
+  @default_count 10
+
   def generate_titles(opts \\ []) do
     Titles.get_many_random_titles(
-      Keyword.get(opts, :count, 10)
+      Keyword.get(opts, :count, @default_count)
     )
   end
 
   def generate_pages(opts \\ []) do
     Pages.get_many_random_pages(
-      Keyword.get(opts, :count, 10)
+      Keyword.get(opts, :count, @default_count)
     )
   end
 end
